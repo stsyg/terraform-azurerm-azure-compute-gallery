a0ea336ac7355b614aaf5f9f171f207d1435b9dc
sergiyt <sergiyt@outlook.com>
2022-06-10 09:23:49 -0600

    initial load of the code

---
+++ main.tf
@@ -0,0 +1,54 @@
+﻿# Terraform Cloud configuration
+terraform {
+        backend "remote" {
+            organization = "The38Dev"
+            workspaces {
+                name = "AzureComputeGallery"
+            }
+        }
+}
+
+# Creates resource group
+resource "azurerm_resource_group" "sigrg" {
+  location = var.deploy_location
+  name     = var.rg_shared_name
+}
+
+# Generates a random string (consisting of four characters)
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+resource "random_string" "rando" {
+  length  = 4
+  upper   = false
+  special = false
+}
+
+
+# Creates Azure Compute Gallery (formerly Shared Image Gallery)
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/shared_image_gallery
+resource "azurerm_shared_image_gallery" "sig" {
+  name                = "sig${random_string.random.id}"
+  resource_group_name = azurerm_resource_group.sigrg.name
+  location            = azurerm_resource_group.sigrg.location
+  description         = "VM images"
+
+  tags = {
+    Environment = "Dev"
+    App         = "Azure Compute Gallery"
+  }
+}
+
+# Creates image definition
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/shared_image
+resource "azurerm_shared_image" "example" {
+  name                = "avd-image"
+  gallery_name        = azurerm_shared_image_gallery.sig.name
+  resource_group_name = azurerm_resource_group.sigrg.name
+  location            = azurerm_resource_group.sigrg.location
+  os_type             = "Windows"
+
+  identifier {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "office-365"
+    sku       = "20h2-evd-o365pp"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+﻿variable "deploy_location" {
+  type        = string
+  default     = "canadacentral"
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "rg_shared_name" {
+  type        = string
+  default     = "rg-azure-compute-gallery"
+  description = "Name of the Resource group in which to deploy shared resources"
+}
