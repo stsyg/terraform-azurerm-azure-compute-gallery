7bfab7e410bab427dec05c153e89c21abaeef7fc
sergiyt <sergiyt@outlook.com>
2022-06-10 09:34:29 -0600

    fixed typo in the resource block

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "azurerm_resource_group" "sigrg" {
 
 # Generates a random string (consisting of four characters)
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-resource "random_string" "rando" {
+resource "random_string" "random" {
   length  = 4
   upper   = false
   special = false
