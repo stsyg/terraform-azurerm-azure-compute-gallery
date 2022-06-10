04c3fd3df1a29a2e1cdac957e58534146e9a4df3
sergiyt <sergiyt@outlook.com>
2022-06-10 16:39:44 -0600

    fixing subscription variable

--- main.tf
+++ main.tf
@@ -102,8 +102,8 @@ resource "azurerm_role_definition" "aibIdentity" {
   }
 
   assignable_scopes = [
-      subscription_id
-#    data.azurerm_subscription.current.id, # /subscriptions/00000000-0000-0000-0000-000000000000
+#      subscription_id
+    data.azurerm_subscription.current.id, # /subscriptions/00000000-0000-0000-0000-000000000000
   ]
 }
 
