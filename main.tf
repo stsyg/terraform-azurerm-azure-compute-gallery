f832d361ec2dd7b1070e94664ec94e2a47e0cdf7
sergiyt <sergiyt@outlook.com>
2022-06-13 09:40:48 -0600

    changed code for managed identity call

--- main.tf
+++ main.tf
@@ -131,7 +131,7 @@ resource "azurerm_role_assignment" "aibIdentityAssignment" {
   scope              = data.azurerm_subscription.current.id
   role_definition_id = azurerm_role_definition.aibIdentity.role_definition_resource_id
 #  principal_id       = data.azurerm_client_config.example.object_id
-  principal_id       = data.azurerm_user_assigned_identity.aib.object_id
+  principal_id       = azurerm_user_assigned_identity.aib.object_id
 }
 
 # # Update image definition version
